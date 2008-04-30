87457e1eaa60284d3d580392b206e32101204fc1
Nino Martinez Wael <nino.martinez.wael@gmail.com>
2008-04-30 12:20:10 +0000

    added simple styling...

--- src/main/java/org/planningpoker/wicket/pages/BasePage.java
+++ src/main/java/org/planningpoker/wicket/pages/BasePage.java
@@ -1,15 +1,24 @@
 package org.planningpoker.wicket.pages;
 
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.markup.html.IHeaderContributor;
+import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 
-public abstract class BasePage extends WebPage {
+public abstract class BasePage extends WebPage implements IHeaderContributor {
+
+	public void renderHead(IHeaderResponse response) {
+		response.renderCSSReference(new ResourceReference(BasePage.class,
+				"style.css"));
+
+	}
 
 	public BasePage() {
 		add(new Label<String>("pageTitle", getPageTitle()));
 	}
-	
+
 	protected abstract IModel<String> getPageTitle();
-	
+
 }
