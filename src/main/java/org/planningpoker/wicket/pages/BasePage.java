f788a3c907cfaf3d32b6e484e49d22a6ce118e80
Frank Bille <github@frankbille.dk>
2008-04-30 12:41:54 +0000

    Merged the two ways of adding the stylesheet.

--- src/main/java/org/planningpoker/wicket/pages/BasePage.java
+++ src/main/java/org/planningpoker/wicket/pages/BasePage.java
@@ -1,19 +1,10 @@
 package org.planningpoker.wicket.pages;
 
-import org.apache.wicket.ResourceReference;
-import org.apache.wicket.markup.html.IHeaderContributor;
-import org.apache.wicket.markup.html.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.model.IModel;
 
-public abstract class BasePage extends WebPage implements IHeaderContributor {
-
-	public void renderHead(IHeaderResponse response) {
-		response.renderCSSReference(new ResourceReference(BasePage.class,
-				"style.css"));
-
-	}
+public abstract class BasePage extends WebPage {
 
 	public BasePage() {
 		add(new Label<String>("pageTitle", getPageTitle()));
